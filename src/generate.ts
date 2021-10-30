2ce51cfd1ad51260565a688e477a251f3984b29e
Patric Lundin <patric.lundin@emma-sleep.com>
2021-10-30 17:04:29 +0200

    feat: Add hooks in generate mode before and after file generation
    **Why** is the change needed?
    
    The current implementation only resolves the options used
    to generate the robots.txt statically from nuxt.config.js.
    This limits the module's use cases as it is not possible to extend
    the config from another nuxt module during static site generation.
    
    **How** is the need addressed?
    
    By using the same pattern as @nuxtjs/sitemap do for this feature
    
    - Calling robots:generate:before hook passing options before file generation
    - Calling robots:generate:done hook after file generation
    
    What **side-effects** could the change have?
    
    - The options can be modified by other nuxt modules during generation

--- src/generate.ts
+++ src/generate.ts
@@ -6,10 +6,12 @@ import { FILE_NAME, getRules, render } from './utils'
 
 export function generate (options: Rule[]) {
   this.nuxt.hook('generate:done', async () => {
+    await this.nuxt.callHook('robots:generate:before', this, options)
     const { rootDir, generate: { dir: generateDir } } = this.options
     const generateFilePath = resolve(rootDir, generateDir, FILE_NAME)
     const rules: RuleInterface[] = await getRules.call(this, options)
 
     writeFileSync(generateFilePath, render([...getStaticRules(), ...rules]))
+    await this.nuxt.callHook('robots:generate:done', this)
   })
 }
