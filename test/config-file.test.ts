acc53bfdca5a1572c7f9ce3c300ac420663b8be5
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-11-01 04:18:44 +0100

    test: demo ignored function parameter

--- test/config-file.test.ts
+++ test/config-file.test.ts
@@ -8,6 +8,11 @@ describe('config file', async () => {
 
   test('render', async () => {
     const body = await $fetch('/robots.txt')
-    expect(body).toBe('User-agent: Googlebot\nUser-agent: Bingbot\n# Comment here\n\nDisallow: /admin')
+    expect(body).toMatch(/User-agent: Googlebot\nUser-agent: Bingbot\n# Comment here\n\nDisallow: \/admin\nSitemap: https:\/\/127\.0\.0\.1:[0-9]+\/sitemap\.xml/)
+  })
+
+  test('render with different host', async () => {
+    const body = await $fetch('/robots.txt', { headers: { Host: 'robots.test:1234' } })
+    expect(body).toBe('User-agent: Googlebot\nUser-agent: Bingbot\n# Comment here\n\nDisallow: /admin\nSitemap: https://robots.test:1234/sitemap.xml')
   })
 })
--- test/fixture/config-file/robots.config.ts
+++ test/fixture/config-file/robots.config.ts
@@ -1,6 +1,9 @@
+import { IncomingMessage } from 'node:http'
+
 export default [
   { UserAgent: () => ['Googlebot', () => 'Bingbot'] },
   { Comment: 'Comment here' },
   { BlankLine: true },
-  { Disallow: '/admin' }
+  { Disallow: '/admin' },
+  { Sitemap: (req: IncomingMessage) => `https://${req.headers.host}/sitemap.xml` }
 ]
