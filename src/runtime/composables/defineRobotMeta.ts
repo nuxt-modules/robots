45d1417fccc80d404aa5974b64acd021b8cafd04
Harlan Wilton <harlan@harlanzw.com>
2023-01-24 12:45:59 +1100

    refactor: use `exposeModuleConfig`

--- src/runtime/composables/defineRobotMeta.ts
+++ src/runtime/composables/defineRobotMeta.ts
@@ -1,6 +1,5 @@
 import { useServerHead } from '@vueuse/head'
-// eslint-disable-next-line import/default
-import config from '#nuxt-simple-robots/config'
+import { indexable, robotsDisabledValue, robotsEnabledValue } from '#nuxt-simple-robots/config'
 import { useNuxtApp } from '#imports'
 
 export function defineRobotMeta() {
@@ -13,8 +12,7 @@ export function defineRobotMeta() {
           content: () => {
             // SSR only
             const routeRules = nuxtApp?.ssrContext?.event?.context?._nitro?.routeRules
-            const key = (config.indexable === false || routeRules?.index === false) ? 'robotsDisabledValue' : 'robotsEnabledValue' as const
-            return config[key]
+            return (indexable === false || routeRules?.index === false) ? robotsDisabledValue : robotsEnabledValue
           },
         },
       ],
--- src/runtime/server/robots-middleware.ts
+++ src/runtime/server/robots-middleware.ts
@@ -1,13 +1,12 @@
 import { defineEventHandler, setHeader } from 'h3'
-import config from 'nuxt-simple-robots/config'
+import { indexable, robotsDisabledValue } from '#nuxt-simple-robots/config'
 import { getRouteRules } from '#internal/nitro'
 
 export default defineEventHandler((event) => {
   if (event.path === '/robots.txt')
     return
   // add noindex header
-  const siteIndexable = config.indexable
   const routeRules = getRouteRules(event)
-  if (routeRules.index === false || siteIndexable === false)
-    setHeader(event, 'X-Robots-Tag', config.robotsDisabledValue)
+  if (routeRules.index === false || indexable === false)
+    setHeader(event, 'X-Robots-Tag', robotsDisabledValue)
 })
