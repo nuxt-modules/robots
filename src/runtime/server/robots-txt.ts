171fc9c7e8fdb8c27a1f75b1b1f49f2a078393f1
Harlan Wilton <harlan@harlanzw.com>
2023-10-30 16:46:07 +0900

    fix: safer `indexable` status with improved dev hints
    Maybe fixes #44

--- src/runtime/server/robots-txt.ts
+++ src/runtime/server/robots-txt.ts
@@ -6,15 +6,13 @@ import { useNitroApp, useRuntimeConfig, useSiteConfig, withSiteUrl } from '#impo
 export default defineEventHandler(async (e) => {
   const query = getQuery(e)
   setHeader(e, 'Content-Type', 'text/plain; charset=utf-8')
-  setHeader(e, 'Cache-Control', process.dev ? 'no-store' : 'max-age=14400, must-revalidate')
+  setHeader(e, 'Cache-Control', import.meta.dev ? 'no-store' : 'max-age=14400, must-revalidate')
 
   const { groups, sitemap, credits } = useRuntimeConfig()['nuxt-simple-robots']
-  const { indexable } = useSiteConfig(e)
+  const { indexable, _context } = useSiteConfig(e, { debug: import.meta.dev })
 
   // allow previewing with ?indexable
   const queryIndexableEnabled = String(query.indexable) === 'true' || query.indexable === ''
-  const isIndexable = (process.dev && queryIndexableEnabled) || indexable !== false
-
   let sitemaps: string[] = [...(Array.isArray(sitemap) ? sitemap : [sitemap])]
     // validate sitemaps are absolute
     .map((s) => {
@@ -26,25 +24,53 @@ export default defineEventHandler(async (e) => {
   // dedupe sitemaps
   sitemaps = [...new Set(sitemaps)]
 
-  let robotGroups: RobotsGroupResolved[] = [...groups]
-  if (!isIndexable) {
-    robotGroups = [
-      {
-        userAgent: ['*'],
-        disallow: ['/'],
-      },
-    ]
-    sitemaps = [] // no point adding sitemaps if not indexable
+  let computedIndexable = indexable
+  const isRobotsGroupConfigBlockingIndexing = (groups as RobotsGroupResolved[]).some(
+    g => g.userAgent.includes('*') && g.disallow.includes('/'),
+  )
+  if (computedIndexable && isRobotsGroupConfigBlockingIndexing)
+    computedIndexable = false
+  const devHints: string[] = []
+  if (import.meta.dev) {
+    if (isRobotsGroupConfigBlockingIndexing) {
+      devHints.push('Robots config is blocking indexing.')
+    }
+    else {
+      if (queryIndexableEnabled && _context.indexable === 'computed-env') {
+        computedIndexable = true
+        devHints.push('You are mocking a production enviroment with ?indexable query.')
+      }
+      else if (!computedIndexable && !indexable) {
+        if (_context.indexable === 'computed-env')
+          devHints.push(`Indexing is blocked because of the environment. You can mock a production environment with ?indexable query.`)
+        else
+          devHints.push(`Indexing is blocked by site config set by ${_context.indexable}.`)
+      }
+    }
   }
+  if (!computedIndexable) {
+    // no point adding sitemaps if not indexable
+    sitemaps = []
+  }
+  const robotGroups: RobotsGroupResolved[] = computedIndexable
+    ? ([...groups] as RobotsGroupResolved[])
+    : [
+        {
+          allow: [],
+          comment: [],
+          userAgent: ['*'],
+          disallow: ['/'],
+        },
+      ]
 
   let robotsTxt: string = generateRobotsTxt({ groups: robotGroups, sitemaps })
-  if (process.dev) {
+  if (import.meta.dev && devHints.length) {
     // append
-    robotsTxt += `\n# Dev mode ${queryIndexableEnabled ? 'Previewing indexable. Remove ?indexable to see the default.' : ' - View the indexable output by setting ?indexable'}` + '\n'
+    robotsTxt += `\n# DEVELOPMENT HINTS: ${devHints.join(', ')}\n`
   }
   if (credits) {
     robotsTxt = [
-      `# START nuxt-simple-robots (${isIndexable ? 'indexable' : 'indexing disabled'})`,
+      `# START nuxt-simple-robots (${computedIndexable ? 'indexable' : 'indexing disabled'})`,
       robotsTxt,
       '# END nuxt-simple-robots',
     ].filter(Boolean).join('\n')
