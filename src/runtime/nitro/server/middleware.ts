2aad9aa527ff190a95e3fafc50caccc5a1abee7a
Harlan Wilton <harlan@harlanzw.com>
2023-12-18 13:47:54 +1100

    fix: skip internal routes when adding robots header

--- src/runtime/nitro/server/middleware.ts
+++ src/runtime/nitro/server/middleware.ts
@@ -1,10 +1,11 @@
 import { defineEventHandler, setHeader } from 'h3'
+import { withoutQuery } from '../kit'
 import { getPathRobotConfig } from '#internal/nuxt-simple-robots'
 
 export default defineEventHandler(async (e) => {
-  if (e.path === '/robots.txt')
+  if (e.path === '/robots.txt' || e.path.startsWith('/__') || e.path.startsWith('/api') || e.path.startsWith('/_nuxt'))
     return
-  const robotConfig = await getPathRobotConfig(e, { path: e.path, context: 'middleware' })
+  const robotConfig = await getPathRobotConfig(e, { path: withoutQuery(e.path), context: 'middleware' })
   setHeader(e, 'X-Robots-Tag', robotConfig.rule)
   e.context.robots = robotConfig
 })
