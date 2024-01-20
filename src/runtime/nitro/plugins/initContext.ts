9e06f364838f3f109cb58153ac9b1d24e0e23fbb
Harlan Wilton <harlan@harlanzw.com>
2024-01-20 23:42:32 +0800

    fix: inject robots meta tag for SPA routes
    Fixes #68

--- src/runtime/nitro/plugins/initContext.ts
+++ src/runtime/nitro/plugins/initContext.ts
@@ -1,7 +1,9 @@
 import type { NitroApp } from 'nitropack'
 import { withoutTrailingSlash } from 'ufo'
 import { resolveRobotsTxtContext } from '../util'
-import { defineNitroPlugin, useRuntimeConfig } from '#imports'
+import { defineNitroPlugin, getRouteRules, useRuntimeConfig } from '#imports'
+
+const PRERENDER_NO_SSR_ROUTES = new Set(['/index.html', '/200.html', '/404.html'])
 
 // we need to init our state using a nitro plugin so the user doesn't throttle the resolve context hook
 // important when we integrate with nuxt-simple-sitemap and we're checking thousands of URLs
@@ -15,4 +17,17 @@ export default defineNitroPlugin(async (nitroApp: NitroApp) => {
     urls.forEach((url: string) => nuxtContentUrls.add(withoutTrailingSlash(url)))
   }
   nitroApp._robots.nuxtContentUrls = nuxtContentUrls
+
+  // need to inject HTML if we have an SPA route
+  nitroApp.hooks.hook('render:html', async (ctx, { event }) => {
+    const routeOptions = getRouteRules(event)
+    const isIsland = (process.env.NUXT_COMPONENT_ISLANDS && event.path.startsWith('/__nuxt_island'))
+    const url = event.path
+    const noSSR = !!(process.env.NUXT_NO_SSR)
+      || event.context.nuxt?.noSSR
+      || (routeOptions.ssr === false && !isIsland)
+      || (import.meta.prerender ? PRERENDER_NO_SSR_ROUTES.has(url) : false)
+    if (noSSR)
+      ctx.head.push(`<meta name="robots" content="${event.context.robots.rule}" />`)
+  })
 })
