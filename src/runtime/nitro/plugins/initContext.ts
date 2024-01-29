7525026daa8eedef7babeee725b548df38e67df3
Harlan Wilton <harlan@harlanzw.com>
2024-01-29 15:11:55 +0800

    fix: add disabled robots value to 404.html
    Fixes #72

--- src/runtime/nitro/plugins/initContext.ts
+++ src/runtime/nitro/plugins/initContext.ts
@@ -8,7 +8,7 @@ const PRERENDER_NO_SSR_ROUTES = new Set(['/index.html', '/200.html', '/404.html'
 // we need to init our state using a nitro plugin so the user doesn't throttle the resolve context hook
 // important when we integrate with nuxt-simple-sitemap and we're checking thousands of URLs
 export default defineNitroPlugin(async (nitroApp: NitroApp) => {
-  const { usingNuxtContent } = useRuntimeConfig()['nuxt-simple-robots']
+  const { usingNuxtContent, robotsDisabledValue } = useRuntimeConfig()['nuxt-simple-robots']
   nitroApp._robots = {} as typeof nitroApp._robots
   await resolveRobotsTxtContext(undefined, nitroApp)
   const nuxtContentUrls = new Set<string>()
@@ -18,19 +18,22 @@ export default defineNitroPlugin(async (nitroApp: NitroApp) => {
   }
   nitroApp._robots.nuxtContentUrls = nuxtContentUrls
 
-  // need to inject HTML if we have an SPA route
-  nitroApp.hooks.hook('render:html', async (ctx, { event }) => {
-    // make sure the route has robots context
-    if (!event.context.robots)
-      return
-    const routeOptions = getRouteRules(event)
-    const isIsland = (process.env.NUXT_COMPONENT_ISLANDS && event.path.startsWith('/__nuxt_island'))
-    const url = event.path
-    const noSSR = !!(process.env.NUXT_NO_SSR)
-      || event.context.nuxt?.noSSR
-      || (routeOptions.ssr === false && !isIsland)
-      || (import.meta.prerender ? PRERENDER_NO_SSR_ROUTES.has(url) : false)
-    if (noSSR)
-      ctx.head.push(`<meta name="robots" content="${event.context.robots.rule}" />`)
-  })
+  if (import.meta.prerender) {
+    // need to inject HTML if we have an SPA route
+    nitroApp.hooks.hook('render:html', async (ctx, { event }) => {
+      const routeOptions = getRouteRules(event)
+      const isIsland = (process.env.NUXT_COMPONENT_ISLANDS && event.path.startsWith('/__nuxt_island'))
+      const noSSR = !!(process.env.NUXT_NO_SSR)
+        || event.context.nuxt?.noSSR
+        || (routeOptions.ssr === false && !isIsland)
+        || (import.meta.prerender ? PRERENDER_NO_SSR_ROUTES.has(event.path) : false)
+      if (noSSR) {
+        let rule = event.context.robots?.rule
+        if (event.path === '/404.html')
+          rule = robotsDisabledValue
+        if (rule)
+          ctx.head.push(`<meta name="robots" content="${rule}" />`)
+      }
+    })
+  }
 })
