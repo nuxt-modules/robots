2e6d78d83b05dd4b17abf019209cbb5c44c96a93
harlan <harlan@harlanzw.com>
2024-08-22 01:35:00 +1000

    chore: lint

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -20,7 +20,6 @@ describe('basic', () => {
 
       # END nuxt-robots"
     `)
-    
 
     const instance = await fetch('/')
     expect(instance.headers.get('x-robots-tag')).toMatchInlineSnapshot(`"index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"`)
