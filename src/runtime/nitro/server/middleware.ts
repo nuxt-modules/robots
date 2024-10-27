ee185e576e6116796c33f58773e1fdd400c541da
Kouji Sano <KoujiSano@users.noreply.github.com>
2024-10-27 16:00:03 +0900

    patch

--- src/runtime/nitro/server/middleware.ts
+++ src/runtime/nitro/server/middleware.ts
@@ -6,9 +6,12 @@ export default defineEventHandler(async (e) => {
   if (e.path === '/robots.txt' || e.path.startsWith('/__') || e.path.startsWith('/api') || e.path.startsWith('/_nuxt'))
     return
   const robotConfig = getPathRobotConfig(e)
-  const { header } = useRuntimeConfig(e)['nuxt-robots']
-  if (header) {
-    setHeader(e, 'X-Robots-Tag', robotConfig.rule)
+  const nuxtRobotsConfig = useRuntimeConfig(e)['nuxt-robots']
+  if (nuxtRobotsConfig) {
+    const { header } = nuxtRobotsConfig
+    if (header) {
+      setHeader(e, 'X-Robots-Tag', robotConfig.rule)
+    }
+    e.context.robots = robotConfig
   }
-  e.context.robots = robotConfig
 })
