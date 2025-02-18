23d28c66b6b655cad250f5d15e5cb0c44bde72e6
harlan <harlan@harlanzw.com>
2025-02-18 14:08:09 +1100

    chore: broken content types

--- src/content.ts
+++ src/content.ts
@@ -1,14 +1,11 @@
 import type { Collection } from '@nuxt/content'
-import type { TypeOf, ZodRawShape } from 'zod'
-import { z } from '@nuxt/content'
+import { z } from 'zod'
 
 export const schema = z.object({
   robots: z.union([z.string(), z.boolean()]).optional(),
 })
 
-export type RobotSchema = TypeOf<typeof schema>
-
-export function asRobotsCollection<T extends ZodRawShape>(collection: Collection<T>): Collection<T> {
+export function asRobotsCollection<T extends Collection<any>>(collection: T): T {
   if (collection.type === 'page') {
     // @ts-expect-error untyped
     collection.schema = collection.schema ? schema.extend(collection.schema.shape) : schema
